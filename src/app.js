914d4bce9ad502fdbbe63dc42fe0812169a67514
supriyaji <yadav.supriya858@gmail.com>
2021-08-11 22:53:32 +0530

    adding script for heroku

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path= require('path');
-const port = 5000;
+const port = process.env.PORT || 5000;
 const hbs = require('hbs');
 
 const static_path = path.join(__dirname,"../public");
